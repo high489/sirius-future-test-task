87f4234c8fb4edb81933db301ddfd63f2b928891
high489 <vitalii.matskaniuk@gmail.com>
2024-06-08 18:33:52 -0300

    Add: react project created based on vite
    Files structure, aliases configured

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import './index.scss'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </>
+  )
+}
+
+export default App
